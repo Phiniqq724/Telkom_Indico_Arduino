afa29a18e88d0788ae05c33bb4866bd3fea59018
phiniqq724 <pickaxee019@gmail.com>
2025-05-01 19:05:08 +0700

    Fourth Commit

--- Indico_MegaToLora/Indico_MegaToLora.ino
+++ Indico_MegaToLora/Indico_MegaToLora.ino
@@ -1,15 +1,14 @@
 #include <LoRa.h>
 #include <ModbusMaster.h>
 #include "EasyNextionLibrary.h"
-#include <Sensirion.h>       
-#include <SensirionSHT.h>    
+#include <Sensirion.h>
+#include <SensirionSHT.h>
 
 // create an instance of SensirionSHT on pins 4 and 5
 SensirionSHT Sensor = SensirionSHT(20, 21);
 
 // set delay to one second
 long delayTime = 1;
-
 
 EasyNex myNex(Serial);
 int currentPage = -1;
@@ -18,7 +17,6 @@ ModbusMaster CO2;
 ModbusMaster NH3;
 ModbusMaster WIND;
 
-
 #define SS_PIN 53
 #define RST_PIN 9
 #define DIO0_PIN 2
@@ -26,12 +24,59 @@ ModbusMaster WIND;
 String device_id = "1";
 String kandang_id = "1_1_3";
 unsigned long lastSendTime = 0;
-unsigned long sendInterval = 1000; // 10 seconds
+unsigned long sendInterval = 1000;
 unsigned long timeElapse = 0;
 
 float nh3ppm = 0;
 float co2ppm = 0;
 float windSpeed = 0;
+
+void updateNextionDisplay(float temperature, float humidity, float co2, float nh3, float wind)
+{
+  String tempStr = String(temperature, 1) + " C";
+  String humStr = String(humidity, 1) + "%";
+  String tempHumStr = tempStr + " / " + humStr;
+  String co2Str = String((int)co2) + " ppm";
+  String nh3Str = String(nh3, 1) + " ppm";
+  String windStr = String(wind, 1) + " m/s";
+
+  switch (currentPage)
+  {
+  case 0:
+    myNex.writeStr("t4.txt", windStr);
+    myNex.writeStr("t1.txt", nh3Str);
+    myNex.writeStr("t2.txt", tempHumStr);
+    myNex.writeStr("t3.txt", co2Str);
+    break;
+
+  case 1:
+    myNex.writeStr("t1.txt", windStr);
+    break;
+
+  case 2:
+    myNex.writeStr("t1.txt", nh3Str);
+    break;
+
+  case 3:
+    myNex.writeStr("t1.txt", tempStr);
+    break;
+
+  case 4:
+    myNex.writeStr("t1.txt", humStr);
+    break;
+
+  case 5:
+    myNex.writeStr("t1.txt", co2Str);
+    break;
+
+  case 6:
+    myNex.writeStr("t4.txt", windStr);
+    myNex.writeStr("t2.txt", tempStr);
+    myNex.writeStr("t1.txt", nh3Str);
+    myNex.writeStr("t3.txt", co2Str);
+    break;
+  }
+}
 
 void setup()
 {
@@ -55,12 +100,12 @@ void setup()
   //     ;
   // }
 
-//   LoRa.setSpreadingFactor(7);
-//   LoRa.setSignalBandwidth(125E3);
-//   LoRa.setCodingRate4(5);
-//   LoRa.setSyncWord(0x12);
-//   LoRa.enableCrc();
- }
+  LoRa.setSpreadingFactor(7);
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setCodingRate4(5);
+  LoRa.setSyncWord(0x12);
+  LoRa.enableCrc();
+}
 
 int checkCurrentPage()
 {
@@ -80,8 +125,8 @@ void loop()
   // put your main code here, to run repeatedly:
   float temperature = Sensor.getTemperature();
   float humidity = Sensor.getHumidity();
-
-
+  myNex.NextionListen();
+  updateNextionDisplay(temperature, humidity, co2ppm, nh3ppm, windSpeed);
   if (millis() - lastSendTime >= sendInterval)
   {
     lastSendTime = millis();
@@ -112,12 +157,11 @@ void loop()
                      ",\"NH3\":" + nh3ppm +
                      ",\"windspeed\":" + windSpeed + "}";
 
-    // // Send data
-    // LoRa.beginPacket();
-    // LoRa.print(payload);
-    // LoRa.endPacket();
+    // Send data
+    LoRa.beginPacket();
+    LoRa.print(payload);
+    LoRa.endPacket();
 
     Serial.println(payload);
-    delay(delayTime * 1000);
   }
 }
